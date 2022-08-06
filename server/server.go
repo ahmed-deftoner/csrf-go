8d43b4c13de5354f2a1ff3fb8a90e92783eb7268
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-06 21:38:26 +0500

    start server

--- server/server.go
+++ server/server.go
@@ -1,5 +1,17 @@
 package server
 
-func StartServer() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/ahmed-deftoner/csrf-go/server/middleware"
+)
+
+func StartServer(hostname string, port string) error {
+	host := hostname + "." + port
+
+	log.Printf("listening on %s", host)
+	handler := middleware.NewHandler()
+	http.Handle("/", handler)
+	return http.ListenAndServe(host, nil)
 }
