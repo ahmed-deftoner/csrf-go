bbc16fb4da3b47e25bc1b7c3797dd3ccb05189e7
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-06 21:27:27 +0500

    generate random string

--- randomstrings/randomstrings.go
+++ randomstrings/randomstrings.go
@@ -1,5 +1,12 @@
 package randomstrings
 
-func GenerateRandomString(int) {
+import "encoding/base64"
 
+func GenerateRandomBytes(n int) ([]byte, error) {
+
+}
+
+func GenerateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	return base64.URLEncoding.EncodeToString(b), err
 }
