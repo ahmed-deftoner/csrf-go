c65a9b1dcc144b05372eaf381e453cf8921bdb1d
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-07 19:14:35 +0500

    new handler

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,1 +1,23 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
+
+func NewHandler() http.Handler {
+	return alice.New(recoverHandler, authHandler).ThenFunc(logicHandler)
+}
+
+func recoverHandler() http.Handler {
+
+}
+
+func authHandler() http.Handler {
+
+}
+
+func logicHandler() {
+
+}
