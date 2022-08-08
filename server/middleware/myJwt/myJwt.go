cb9eb664743d781eb549b93011965403d385590e
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-08 17:47:08 +0500

    myjwt outline

--- server/middleware/myJwt/myJwt.go
+++ server/middleware/myJwt/myJwt.go
@@ -5,3 +5,39 @@ import "errors"
 func JWTInit() error {
 	return errors.New("error")
 }
+
+func CreateNewTokens() {
+
+}
+
+func CheckAndRefreshTokens() {
+
+}
+
+func CreateAuthTokenString() {
+
+}
+
+func createRefreshTokenString(uuid string, role string, csrfString string) (refreshTokenString string, err error) {
+
+}
+
+func updateRefreshTokenExp(oldRefreshTokenString string) (newRefreshTokenString string, err error) {
+
+}
+
+func updateAuthTokenString(refreshTokenString string, oldAuthTokenString string) (newAuthTokenString, csrfSecret string, err error) {
+
+}
+
+func RevokeRefreshToken(refreshTokenString string) error {
+
+}
+
+func updateRefreshTokenCsrf(oldRefreshTokenString string, newCsrfString string) (newRefreshTokenString string, err error) {
+
+}
+
+func GrabUUID(authTokenString string) (string, error) {
+
+}
