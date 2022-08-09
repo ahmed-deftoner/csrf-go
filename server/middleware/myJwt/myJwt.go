de80b14ae240c1e0613a4dc1318cabeb5d04146b
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-09 20:53:15 +0500

    jwt init

--- server/middleware/myJwt/myJwt.go
+++ server/middleware/myJwt/myJwt.go
@@ -1,14 +1,38 @@
 package myjwt
 
-import "errors"
+import (
+	"io/ioutil"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 const (
-	privKeyPath = "keys/app.rsa"
-	pubKeyPath  = "keys/app.rsa.pub"
+	privKeyPath = "keys/id_rsa"
+	pubKeyPath  = "keys/id_rsa.pub"
 )
 
 func JWTInit() error {
-	return errors.New("error")
+	signBytes, err := ioutil.ReadFile(privKeyPath)
+	if err != nil {
+		return err
+	}
+
+	signKey, err = jwt.ParseRSAPrivateKeyFromPEM(signBytes)
+	if err != nil {
+		return err
+	}
+
+	verifyBytes, err := ioutil.ReadFile(pubKeyPath)
+	if err != nil {
+		return err
+	}
+
+	verifyKey, err = jwt.ParseRSAPublicKeyFromPEM(verifyBytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func CreateNewTokens() {
