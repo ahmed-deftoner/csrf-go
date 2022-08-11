a4407603d725de4bd0c213dac18ade76af90f4e6
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-11 21:07:37 +0500

    parse template

--- server/templates/templates.go
+++ server/templates/templates.go
@@ -1,1 +1,5 @@
 package templates
+
+import "text/template"
+
+var templates = template.Must(template.ParseFiles("./server/templates/templateFiles/login.tmpl", "./server/templates/templateFiles/register.tmpl", "./server/templates/templateFiles/restricted.tmpl"))
