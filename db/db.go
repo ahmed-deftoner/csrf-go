ebd155548cc93d25c4b31dad2ac0cc98166398e8
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-13 19:06:38 +0500

    db funcs

--- db/db.go
+++ db/db.go
@@ -7,3 +7,43 @@ var users = map[string]models.User{}
 func DBInit() {
 
 }
+
+func StoreUser(username string, password string, role string) (uuid string, err error) {
+
+}
+
+func DeleteUser(uuid string) {
+
+}
+
+func FetchUserById(uuid string) (models.User, error) {
+
+}
+
+func FetchUserByUsername(username string) (models.User, string, error) {
+
+}
+
+func StoreRefreshToken() (jti string, err error) {
+
+}
+
+func DeleteRefreshToken(jti string) {
+
+}
+
+func CheckRefreshToken(jti string) bool {
+
+}
+
+func LogUserIn(username string, password string) (models.User, string, error) {
+
+}
+
+func generateBcryptHash(password string) (string, error) {
+
+}
+
+func checkPasswordAgainstHash(hash string, password string) error {
+
+}
